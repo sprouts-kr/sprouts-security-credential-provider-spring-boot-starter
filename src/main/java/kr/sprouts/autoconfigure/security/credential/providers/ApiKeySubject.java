9ea77287236afae95ed8e91c7bcfec13026cc3a4
Jinwon Seo <jwseo@live.co.kr>
2023-08-10 14:45:26 +0900

    ApiKeyCredentialProvider 추가. Closed #3

---
+++ src/main/java/kr/sprouts/autoconfigure/security/credential/providers/ApiKeySubject.java
@@ -0,0 +1,18 @@
+package kr.sprouts.autoconfigure.security.credential.providers;
+
+import kr.sprouts.security.credential.Subject;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiKeySubject extends Subject {
+    private ApiKeySubject(UUID memberId) {
+        super(memberId);
+    }
+
+    public static ApiKeySubject of(UUID memberId) {
+        return new ApiKeySubject(memberId);
+    }
+}
